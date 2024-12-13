45d3b4501415a07c09e6cb865c060be7354bcaa3
Stellakoomson <stellakoomson006@gmail.com>
2024-12-13 13:57:43 +0000

    adding all files

---
+++ src/index.js
@@ -0,0 +1,16 @@
+function checkGender(){
+
+let d = prompt("Male or Female");
+if (d== "male"){
+alert(`you are a boy`)
+}
+ 
+else if (d == "female"){
+    alert(`you are a girl`)
+}
+
+
+
+}
+
+checkGender();
